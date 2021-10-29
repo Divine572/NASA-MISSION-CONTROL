64fea293a8a344d92c913d3e8fef008dc42360c7
divine favour <divinef837@gmail.com>
2021-10-29 14:24:05 +0100

    added planets model

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,23 @@
 const http = require('http');
+const { start } = require('repl');
 
 const app = require('./app');
+
+const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
-});
+async function startServer() {
+
+    await loadPlanetsData;
+    
+    server.listen(PORT, () => {
+        console.log(`Listening on PORT ${PORT}`);
+    });
+}
+
+
+
+startServer();
