abbf42834e7605f54792f9760834388c0ca7a09c
divine favour <divinef837@gmail.com>
2021-10-29 15:26:07 +0100

    added authomatated script

--- server/src/app.js
+++ server/src/app.js
@@ -8,10 +8,14 @@ app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
 
 
 
 app.use('/planets', planetsRouter);
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 
 module.exports = app;
