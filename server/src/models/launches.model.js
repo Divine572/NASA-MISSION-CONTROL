5d8412e5e2f1dd8d2b3fde8ecb626500d3c5848a
divine favour <divinef837@gmail.com>
2021-11-05 17:30:15 +0100

    finished launches routes and controllers

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,20 +1,54 @@
 const launches = new Map();
 
+let latestFlightNumber = 100;
+
 const  launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
     rocket: 'Explorer 1S1',
     launchDate: new Date('June 27, 2030'),
-    destination: 'Kepler-442 b',
-    customer: ['ZTM', 'NASA'],
+    target: 'Kepler-442 b',
+    customers: ['ZTM', 'NASA'],
     upcoming: true,
     success: true
 };
 
 launches.set(launch.flightNumber, launch);
 
+function getAllLaunches() {
+    return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+    latestFlightNumber++;
+    launches.set(
+        latestFlightNumber, 
+        Object.assign(launch, {
+            customers: ['ZTM', 'NASA'],
+            flightNumber: latestFlightNumber,
+            upcoming: true,
+            success: true
+        })
+    );
+}
+
+
+function existsLaunchWithId(launchId) {
+    return launches.has(launchId);
+}
+
+
+function abortLaunchById(launchId) {
+    const aborted = launches.get(launchId);
+    aborted.upcoming = false;
+    aborted.success = false;
+    return aborted;
+}
 
 module.exports = {
-    launches,
+    existsLaunchWithId,
+    getAllLaunches,
+    addNewLaunch,
+    abortLaunchById,
 };
 
